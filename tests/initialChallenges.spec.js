14bea49b41ea7504ec67dca3431434ba5f6a1efe
Yuut <ingride.reis.b@gmail.com>
2022-05-30 23:13:50 -0300

    Mostra apenas os ids dos 5 últimos registros da tabela products (a ordernação deve é  baseada na coluna id

--- tests/initialChallenges.spec.js
+++ tests/initialChallenges.spec.js
@@ -67,7 +67,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.only("6 - Exiba os dados das colunas 'product_name' e 'id' da tabela 'products' de maneira que os resultados estejam em ordem alfabética dos nomes", () => {
+  describe("6 - Exiba os dados das colunas 'product_name' e 'id' da tabela 'products' de maneira que os resultados estejam em ordem alfabética dos nomes", () => {
     it('Verifica o desafio6', async () => {
       const challengeQuery = readFileSync('desafio6.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult6');
@@ -85,7 +85,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe("8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'", () => {
+  describe.only("8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'", () => {
     it('Verifica o desafio8', async () => {
       const challengeQuery = readFileSync('desafio8.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult8');
