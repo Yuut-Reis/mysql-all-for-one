49e7feddc3dc2e4eb9cde9a3639f4ce0d853d3d8
Yuut <ingride.reis.b@gmail.com>
2022-05-30 23:53:51 -0300

    Mostra todos os dados da tabela purchase_orders em ordem decrescente, ordenados por created_by em que o created_by é maior ou igual a 3.

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -22,7 +22,7 @@ describe('Desafios sobre filtragem de dados', () => {
     sequelize.close();
   });
 
-  describe.only("9 - Mostre todos os valores da coluna 'notes' da tabela 'purchase_orders' que não são nulos", () => {
+  describe("9 - Mostre todos os valores da coluna 'notes' da tabela 'purchase_orders' que não são nulos", () => {
     it('Verifica o desafio9', async () => {
       const challengeQuery = readFileSync('desafio9.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult9');
@@ -31,7 +31,7 @@ describe('Desafios sobre filtragem de dados', () => {
     });
   });
 
-  describe("10 - Mostre todos os dados da tabela 'purchase_orders' em ordem decrescente ordenados por 'created_by' em que o 'created_by' é maior ou igual a 3", () => {
+  describe.only("10 - Mostre todos os dados da tabela 'purchase_orders' em ordem decrescente ordenados por 'created_by' em que o 'created_by' é maior ou igual a 3", () => {
     it('Verifica o desafio10', async () => {
       const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
